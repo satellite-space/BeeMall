8f9c50cb87e9ccfbe2a239abedd59836a021df98
wxy <1875831612@qq.com>
2021-02-02 22:02:51 +0800

    提交gateway组件集成代码

--- mall-gateway/src/main/java/com/bee/mall/gateway/GatewayApplication.java
+++ mall-gateway/src/main/java/com/bee/mall/gateway/GatewayApplication.java
@@ -1,3 +1,5 @@
+package com.bee.mall.gateway;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
--- mall-gateway/src/main/java/com/bee/mall/gateway/controller/NacosController.java
+++ mall-gateway/src/main/java/com/bee/mall/gateway/controller/NacosController.java
@@ -1,10 +1,9 @@
-package controller;
+package com.bee.mall.gateway.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
